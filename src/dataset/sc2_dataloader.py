fed4ccda6a5d9be55efdda6fa66e915e989e8fb1
Andrzej Białecki <kaszanas@interia.pl>
2022-03-15 22:45:20 +0100

    Extended the SC2EGSetDataset capabilities

--- src/dataset/sc2_dataloader.py
+++ src/dataset/sc2_dataloader.py
@@ -1,21 +1,45 @@
-from turtle import down
 from typing import Any
 from torch.utils.data._typing import T_co
 from torch.utils.data import Dataset, DataLoader
+import os
 
 
 class SC2EGSetDataset(Dataset):
-    def __init__(self, dataset_dir: str, url: str, transform=None):
+    def __init__(
+        self,
+        dataset_unpack_dir: str = "./data/unpack",
+        dataset_download_dir: str = "./data/download",
+        url: str = "",
+        transform=None,
+    ):
 
-        self.dataset_dir = dataset_dir
+        self.dataset_download_dir = dataset_download_dir
+        self.dataset_unpack_dir = dataset_unpack_dir
         self.transform = transform
         self.url = url
 
-        self.dataset = None
-
         self.downloaded = False
+        # If there are files in the dataset_unpack_dir it means that it was downloaded and extracted:
+        dataset_unpacked_files = os.listdir(self.dataset_unpack_dir)
+        if dataset_unpacked_files:
+            self.downloaded = True
+
+        # We have received an URL for the dataset and it was not downloaded:
+        if url != "" and not self.downloaded:
+            # TODO: Download the dataset to the dataset_download_dir
+
+            self.downloaded = True
 
         # TODO: Try to unpack all of the zip files that constitute the dataset.
+        # If the directory is correctly specified:
+        if os.path.isdir(dataset_download_dir):
+            # And there are files in the dataset directory:
+            dataset_packed_files = os.listdir(dataset_download_dir)
+            if dataset_packed_files and not dataset_unpacked_files:
+                # Unpack all of the .zip files in that directory:
+                # Create it if it doesnt exist
+
+                self.downloaded = True
 
     def ensure_downloaded(self):
         if self.downloaded:
@@ -40,6 +64,7 @@ class SC2EGSetDataset(Dataset):
     def __getitem__(self, index: Any) -> T_co:
         # TODO: Implement how to get a single item from the dataset!
         # Most likely this will have to load the JSON file.
+        self.ensure_downloaded()
 
         # load and return the specific file that corresponds to the index on the list of files
 
