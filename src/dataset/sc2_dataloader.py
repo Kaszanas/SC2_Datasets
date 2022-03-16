90a81ed72104d71067a99c1b4878e63bd1787235
Andrzej Białecki <kaszanas@interia.pl>
2022-03-16 20:19:27 +0100

    Drafted SC2ReplayData, more logic in SC2EGSetDataset

--- src/dataset/sc2_dataloader.py
+++ src/dataset/sc2_dataloader.py
@@ -3,6 +3,19 @@ from torch.utils.data._typing import T_co
 from torch.utils.data import Dataset, DataLoader
 import os
 import requests
+import uuid
+import zipfile
+
+# TODO: This should hold the extraction logs.
+# And other information that comes out of the processing pipeline:
+class SC2ReplayData:
+    """
+    Holds IterableDatasets for the data that
+    is within a single StarCraft .json representation of a replay file.
+    """
+
+    def __init__(self, economy_stats, created_structures) -> None:
+        pass
 
 
 class SC2EGSetDataset(Dataset):
@@ -31,14 +44,18 @@ class SC2EGSetDataset(Dataset):
         self.dataset_unpack_dir = dataset_unpack_dir
         self.transform = transform
         self.list_of_urls = list_of_urls
+        # Holds all of the filepaths to .zip packages that contain the dataset:
+        self.downloaded_filepaths = []
 
         self.downloaded = False
-        # If there are files in the dataset_unpack_dir it means that it was downloaded and extracted:
+        # If there are files in the dataset_unpack_dir
+        # it means that it was downloaded and extracted:
         dataset_unpacked_files = os.listdir(self.dataset_unpack_dir)
         if dataset_unpacked_files:
             self.downloaded = True
 
-        # We have received an URL for the dataset and it was not downloaded:
+        # We have received an URL for the dataset
+        # and it migth not have been downloaded:
         if list_of_urls:
             self.ensure_downloaded()
 
@@ -57,6 +74,23 @@ class SC2EGSetDataset(Dataset):
         """
         Implements unpacking logic for the dataset.
         """
+        # Unpacking the zip files that were downloaded:
+        for downloaded_zip_path, downloaded_zip_name in self.downloaded_filepaths:
+            with zipfile.ZipFile(downloaded_zip_path, "r") as zip_file:
+                path_to_extract = os.path.join(
+                    self.dataset_unpack_dir, downloaded_zip_name
+                )
+                # Checking the existence of the extraction output directory:
+                if not os.path.exists(path_to_extract):
+                    os.makedirs(path_to_extract)
+                zip_file.extractall(path_to_extract)
+
+                # TODO: Lazily load SC2ReplayData so that it holds the paths
+                # To the .json files holding different info.
+
+        # Load to the list of files
+        # Add the files to the list of files.
+
         pass
 
     def ensure_downloaded(self):
@@ -66,19 +100,28 @@ class SC2EGSetDataset(Dataset):
         if self.downloaded:
             return
 
-        # TODO: download and unpack:
+        # Download all of the files from the list of provided urls:
         for url in self.list_of_urls:
             response = requests.get(self.url)
-            # Get the filename from URL!
-            with open(output_filepath, "wb") as output_map_file:
+            # Get the filename from URL or it needs to be hardcoded!
+            zip_filename = uuid.uuid4().hex
+            download_filepath = os.path.join(
+                self.dataset_download_dir, zip_filename + ".zip"
+            )
+            with open(download_filepath, "wb") as output_map_file:
                 output_map_file.write(response.content)
-            # Load to the list of files
+                self.downloaded_filepaths.append((download_filepath, zip_filename))
+
+        # Unpack the downloaded files:
+        self.__unpack_files()
 
         self.downloaded = True
         return
 
     def __len__(self):
-        """Returns the number of items that are within the dataset"""
+        """
+        Returns the number of items that are within the dataset
+        """
         # TODO: Implement how to calculate the len of the dataset.
         # In the case of SC2EGSet this will be the number of files.
         self.ensure_downloaded()
@@ -88,7 +131,8 @@ class SC2EGSetDataset(Dataset):
         pass
 
     def __getitem__(self, index: Any) -> T_co:
-        """_summary_
+        """
+        Implements the dataset[index] acquisition of data.
 
         :param index: Specifies the index of an item that should be retrieved.
         :type index: Any
@@ -100,5 +144,8 @@ class SC2EGSetDataset(Dataset):
         self.ensure_downloaded()
 
         # load and return the specific file that corresponds to the index on the list of files
+
+        # This should return contents of a .json file?
+        # Or maybe different IterableDatasets that coincide with the Lists that .json holds?
 
         pass
