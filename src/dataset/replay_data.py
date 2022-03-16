659eb3fc51e66437d01f7a380716e756a779cfba
Andrzej Białecki <kaszanas@interia.pl>
2022-03-16 23:38:23 +0100

    Drafted logic

--- src/dataset/replay_data.py
+++ src/dataset/replay_data.py
@@ -1,3 +1,8 @@
+import json
+
+
 class SC2ReplayData:
-    def __init__(self, economy_data, player_info) -> None:
-        pass
+    def __init__(self, replay_filepath: str) -> None:
+        self.replay_filepath = replay_filepath
+
+        # TODO: Read JSON file and load info to class
