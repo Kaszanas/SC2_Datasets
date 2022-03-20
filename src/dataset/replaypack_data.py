f74cbd4d25abd4250af81a56c17ab7eb560eaa8d
Andrzej Białecki <kaszanas@interia.pl>
2022-03-20 23:08:07 +0100

    Changes after code review

--- src/dataset/replaypack_data.py
+++ src/dataset/replaypack_data.py
@@ -1,7 +1,42 @@
-from torch.utils.data import Dataset
 import os
+import uuid
+import requests
 
+from torch.utils.data import Dataset
 from dataset.replay_data import SC2ReplayData
+import zipfile
+
+
+def download_replaypack(
+    destination_dir: str, replaypack_name: str, replaypack_url: str
+) -> str:
+    existing_files = os.listdir(destination_dir)
+
+    if len(existing_files) > 1:
+        raise Exception()
+
+    if existing_files:
+        return existing_files[0]
+
+    response = requests.get(url=replaypack_url)
+    download_filepath = os.path.join(destination_dir, replaypack_name + ".zip")
+    with open(download_filepath, "wb") as output_map_file:
+        output_map_file.write(response.content)
+
+    return download_filepath
+
+
+# Unpacks zip file at zip_path to a destination directory, into a subdirectory.
+def unpack_zipfile(destination_dir: str, subdir: str, zip_path: str):
+    with zipfile.ZipFile(zip_path, "r") as zip_file:
+        path_to_extract = os.path.join(destination_dir, subdir)
+        # Checking the existence of the extraction output directory:
+        if not os.path.exists(path_to_extract):
+            os.makedirs(path_to_extract)
+        zip_file.extractall(path_to_extract)
+
+        return path_to_extract
+
 
 # TODO: This should hold the extraction logs.
 # And other information that comes out of the processing pipeline:
@@ -11,24 +46,41 @@ class SC2ReplaypackData(Dataset):
     is within a single StarCraft .json representation of a replay file.
     """
 
-    def __init__(self, replaypack_directory: str) -> None:
+    def __init__(self, replaypack_directory: str, url="", download=False):
         self.replaypack_directory = replaypack_directory
-        self.list_of_files = []
-        self.len = 0
+
+        replaypack_name = uuid.uuid4().hex
+
+        if download:
+            # Get the name from URL or it needs to be hardcoded!
+            download_path = download_replaypack(
+                replaypack_directory + "/.download/", replaypack_name, url
+            )
+
+            replaypack_path = unpack_zipfile(
+                destination_dir=replaypack_directory,
+                subdir=replaypack_name,
+                zip_path=download_path,
+            )
+
+            # TODO: find data files and unpack
+            replaypack_files = os.listdir(replaypack_path)
+            data_path = None
+
+            for file in replaypack_files:
+                if file.endswith("_data.zip"):
+                    data_path = unpack_zipfile(
+                        destination_dir=replaypack_directory,
+                        subdir="data",
+                        zip_path=file,
+                    )
+
         # Load all of the files
-        if os.path.exists(self.replaypack_directory) and os.path.isdir(
-            self.replaypack_directory
-        ):
-            self.list_of_files = os.listdir(self.replaypack_directory)
-            self.len = len(self.list_of_files)
+        self.list_of_files = os.listdir(data_path)
+        self.len = len(self.list_of_files)
 
     def __len__(self) -> int:
         return self.len
 
-    def __getitem__(self, index: int) -> "SC2ReplayData":
-
-        # TODO: Load the data from the json file and return SC2ReplayData
-        replay_filepath = self.list_of_files[index]
-        sc2_replay_data = SC2ReplayData(replay_filepath=replay_filepath)
-
-        return sc2_replay_data
+    def __getitem__(self, index: int) -> SC2ReplayData:
+        return SC2ReplayData(replay_filepath=self.list_of_files[index])
