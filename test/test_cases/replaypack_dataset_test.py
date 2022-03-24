4313b56e2a2fb2213873b364ebc12ee81d6a8114
Andrzej Białecki <kaszanas@interia.pl>
2022-03-24 22:40:49 +0100

    Drafted contents of tests, changed name of a file

--- test/test_cases/replaypack_dataset_test.py
+++ test/test_cases/replaypack_dataset_test.py
@@ -1,1 +1,10 @@
 import unittest
+
+
+class SC2ReplaypackDatasetTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        return super().setUpClass()
+
+    def test_loading_replaypack(self):
+        pass
