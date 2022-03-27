78bd435da7083f111a5ee5c56a5c7cb1092ed7b3
Andrzej Białecki <kaszanas@interia.pl>
2022-03-27 20:08:44 +0200

    Added player_stats

---
+++ src/dataset/replay_structures/tracker_events/player_stats/stats.py
@@ -0,0 +1,82 @@
+class Stats:
+    def __init__(
+        self,
+        foodMade: int,
+        foodUsed: int,
+        mineralsCollectionRate: int,
+        mineralsCurrent: int,
+        mineralsFriendlyFireArmy: int,
+        mineralsFriendlyFireEconomy: int,
+        mineralsFriendlyFireTechnology: int,
+        mineralsKilledArmy: int,
+        mineralsKilledEconomy: int,
+        mineralsKilledTechnology: int,
+        mineralsLostArmy: int,
+        mineralsLostEconomy: int,
+        mineralsLostTechnology: int,
+        mineralsUsedActiveForces: int,
+        mineralsUsedCurrentArmy: int,
+        mineralsUsedCurrentEconomy: int,
+        mineralsUsedCurrentTechnology: int,
+        mineralsUsedInProgressArmy: int,
+        mineralsUsedInProgressEconomy: int,
+        mineralsUsedInProgressTechnology: int,
+        vespeneCollectionRate: int,
+        vespeneCurrent: int,
+        vespeneFriendlyFireArmy: int,
+        vespeneFriendlyFireEconomy: int,
+        vespeneFriendlyFireTechnology: int,
+        vespeneKilledArmy: int,
+        vespeneKilledEconomy: int,
+        vespeneKilledTechnology: int,
+        vespeneLostArmy: int,
+        vespeneLostEconomy: int,
+        vespeneLostTechnology: int,
+        vespeneUsedActiveForces: int,
+        vespeneUsedCurrentArmy: int,
+        vespeneUsedCurrentEconomy: int,
+        vespeneUsedCurrentTechnology: int,
+        vespeneUsedInProgressArmy: int,
+        vespeneUsedInProgressEconomy: int,
+        vespeneUsedInProgressTechnology: int,
+        workersActiveCount: int,
+    ) -> None:
+        self.foodMade = foodMade
+        self.foodUsed = foodUsed
+        self.mineralsCollectionRate = mineralsCollectionRate
+        self.mineralsCurrent = mineralsCurrent
+        self.mineralsFriendlyFireArmy = mineralsFriendlyFireArmy
+        self.mineralsFriendlyFireEconomy = mineralsFriendlyFireEconomy
+        self.mineralsFriendlyFireTechnology = mineralsFriendlyFireTechnology
+        self.mineralsKilledArmy = mineralsKilledArmy
+        self.mineralsKilledEconomy = mineralsKilledEconomy
+        self.mineralsKilledTechnology = mineralsKilledTechnology
+        self.mineralsLostArmy = mineralsLostArmy
+        self.mineralsLostEconomy = mineralsLostEconomy
+        self.mineralsLostTechnology = mineralsLostTechnology
+        self.mineralsUsedActiveForces = mineralsUsedActiveForces
+        self.mineralsUsedCurrentArmy = mineralsUsedCurrentArmy
+        self.mineralsUsedCurrentEconomy = mineralsUsedCurrentEconomy
+        self.mineralsUsedCurrentTechnology = mineralsUsedCurrentTechnology
+        self.mineralsUsedInProgressArmy = mineralsUsedInProgressArmy
+        self.mineralsUsedInProgressEconomy = mineralsUsedInProgressEconomy
+        self.mineralsUsedInProgressTechnology = mineralsUsedInProgressTechnology
+        self.vespeneCollectionRate = vespeneCollectionRate
+        self.vespeneCurrent = vespeneCurrent
+        self.vespeneFriendlyFireArmy = vespeneFriendlyFireArmy
+        self.vespeneFriendlyFireEconomy = vespeneFriendlyFireEconomy
+        self.vespeneFriendlyFireTechnology = vespeneFriendlyFireTechnology
+        self.vespeneKilledArmy = vespeneKilledArmy
+        self.vespeneKilledEconomy = vespeneKilledEconomy
+        self.vespeneKilledTechnology = vespeneKilledTechnology
+        self.vespeneLostArmy = vespeneLostArmy
+        self.vespeneLostEconomy = vespeneLostEconomy
+        self.vespeneLostTechnology = vespeneLostTechnology
+        self.vespeneUsedActiveForces = vespeneUsedActiveForces
+        self.vespeneUsedCurrentArmy = vespeneUsedCurrentArmy
+        self.vespeneUsedCurrentEconomy = vespeneUsedCurrentEconomy
+        self.vespeneUsedCurrentTechnology = vespeneUsedCurrentTechnology
+        self.vespeneUsedInProgressArmy = vespeneUsedInProgressArmy
+        self.vespeneUsedInProgressEconomy = vespeneUsedInProgressEconomy
+        self.vespeneUsedInProgressTechnology = vespeneUsedInProgressTechnology
+        self.workersActiveCount = workersActiveCount
