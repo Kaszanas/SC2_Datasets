9016c1df5b134c7c2b5e05d42094dde4b72ca65d
Andrzej Białecki <kaszanas@interia.pl>
2022-03-28 00:23:15 +0200

    Changed directory structure

--- src/dataset/replay_structures/toon_player_desc_map/toon_player_desc_map.py
+++ src/dataset/replay_structures/toon_player_desc_map/toon_player_desc_map.py
@@ -5,5 +5,16 @@ from src.dataset.replay_structures.toon_player_desc_map.toon_player_info import 
 
 
 class ToonPlayerDescMap:
+    """
+    _summary_
+
+    :param toon: _description_
+    :type toon: Toon
+    :param toon_player_info: _description_
+    :type toon_player_info: ToonPlayerInfo
+    """
+
     def __init__(self, toon: Toon, toon_player_info: ToonPlayerInfo) -> None:
-        pass
+
+        self.toon = toon
+        self.toon_player_info = toon_player_info
