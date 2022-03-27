5fdaeb3b5a149d38d53d80ac39cf1c0f8642c159
Andrzej Białecki <kaszanas@interia.pl>
2022-03-27 23:56:52 +0200

    Drafted toon_player_desc_map

--- src/dataset/replay_structures/toon_player_desc_map/toon_player_desc_map.py
+++ src/dataset/replay_structures/toon_player_desc_map/toon_player_desc_map.py
@@ -1,3 +1,9 @@
+from src.dataset.replay_structures.toon_player_desc_map.toon import Toon
+from src.dataset.replay_structures.toon_player_desc_map.toon_player_info import (
+    ToonPlayerInfo,
+)
+
+
 class ToonPlayerDescMap:
-    def __init__(self) -> None:
+    def __init__(self, toon: Toon, toon_player_info: ToonPlayerInfo) -> None:
         pass
