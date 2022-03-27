2552e87d0b93d6bd8ea5c5522bab7ef2b870be86
Andrzej Białecki <kaszanas@interia.pl>
2022-03-27 23:24:34 +0200

    Changed date in LICENSE, drafted docstring in stats

--- src/dataset/replay_structures/tracker_events/player_stats/player_stats.py
+++ src/dataset/replay_structures/tracker_events/player_stats/player_stats.py
@@ -3,6 +3,19 @@ from src.dataset.replay_structures.tracker_events.player_stats.stats import Stat
 
 
 class PlayerStats:
+    """
+    PlayerStats holds information about player economy
+
+    :param id: Specifies the ID of an event which corresponds to its name.
+    :type id: int
+    :param loop: Specifies the time at which the event happened in gameloops.
+    :type loop: int
+    :param playerId: Specifies the id of a player to which this event pertains.
+    :type playerId: int
+    :param stats: Specifies a custom data type holding the statistics.
+    :type stats: Stats
+    """
+
     def __init__(self, id: int, loop: int, playerId: int, stats: Stats) -> None:
         self.id = id
         self.loop = loop
