a3e11864b7b0dd18ad4c5f46fc0215570185f109
Andrzej Białecki <kaszanas@interia.pl>
2022-03-28 00:23:15 +0200

    Changed directory structure

--- src/dataset/replay_structures/metadata/metadata.py
+++ src/dataset/replay_structures/metadata/metadata.py
@@ -1,68 +1,29 @@
 class Metadata:
     """_summary_
 
-    :param base_build: _description_
-    :type base_build: str
-    :param data_build: _description_
-    :type data_build: str
-    :param duration_seconds: _description_
-    :type duration_seconds: int
-    :param game_version: _description_
-    :type game_version: str
-    :param map_name: _description_
-    :type map_name: str
+    :param baseBuild: _description_
+    :type baseBuild: str
+    :param dataBuild: _description_
+    :type dataBuild: str
+    :param durationSeconds: _description_
+    :type durationSeconds: int
+    :param gameVersion: _description_
+    :type gameVersion: str
+    :param mapName: _description_
+    :type mapName: str
     """
 
     def __init__(
         self,
-        base_build: str,
-        data_build: str,
-        duration_seconds: int,
-        game_version: str,
-        map_name: str,
+        baseBuild: str,
+        dataBuild: str,
+        durationSeconds: int,
+        gameVersion: str,
+        mapName: str,
     ) -> None:
-        self.baseBuild = base_build
-        self.dataBuild = data_build
-        self.durationSeconds = duration_seconds
-        self.gameVersion = game_version
-        self.mapName = map_name
 
-    @property
-    def base_build(self):
-        return self.base_build
-
-    @property
-    def baseBuild(self):
-        return self.baseBuild
-
-    @property
-    def data_build(self):
-        return self.data_build
-
-    @property
-    def dataBuild(self):
-        return self.dataBuild
-
-    @property
-    def duration_seconds(self):
-        return self.duration_seconds
-
-    @property
-    def durationSeconds(self):
-        return self.durationSeconds
-
-    @property
-    def game_version(self):
-        return self.game_version
-
-    @property
-    def gameVersion(self):
-        return self.gameVersion
-
-    @property
-    def map_name(self):
-        return self.map_name
-
-    @property
-    def mapName(self):
-        return self.mapName
+        self.baseBuild = baseBuild
+        self.dataBuild = dataBuild
+        self.durationSeconds = durationSeconds
+        self.gameVersion = gameVersion
+        self.mapName = mapName
