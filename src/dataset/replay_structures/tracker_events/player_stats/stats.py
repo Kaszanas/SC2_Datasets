dc932b55ab999ab3ee40f9308febac93ef50e768
Andrzej Białecki <kaszanas@interia.pl>
2022-03-27 20:13:17 +0200

    Commented unused code, drafted docstring in stats

--- src/dataset/replay_structures/tracker_events/player_stats/stats.py
+++ src/dataset/replay_structures/tracker_events/player_stats/stats.py
@@ -1,4 +1,87 @@
 class Stats:
+
+    """_summary_
+
+    :param foodMade: _description_
+    :type foodMade: int
+    :param foodUsed: _description_
+    :type foodUsed: int
+    :param mineralsCollectionRate: _description_
+    :type mineralsCollectionRate: int
+    :param mineralsCurrent: _description_
+    :type mineralsCurrent: int
+    :param mineralsFriendlyFireArmy: _description_
+    :type mineralsFriendlyFireArmy: int
+    :param mineralsFriendlyFireEconomy: _description_
+    :type mineralsFriendlyFireEconomy: int
+    :param mineralsFriendlyFireTechnology: _description_
+    :type mineralsFriendlyFireTechnology: int
+    :param mineralsKilledArmy: _description_
+    :type mineralsKilledArmy: int
+    :param mineralsKilledEconomy: _description_
+    :type mineralsKilledEconomy: int
+    :param mineralsKilledTechnology: _description_
+    :type mineralsKilledTechnology: int
+    :param mineralsLostArmy: _description_
+    :type mineralsLostArmy: int
+    :param mineralsLostEconomy: _description_
+    :type mineralsLostEconomy: int
+    :param mineralsLostTechnology: _description_
+    :type mineralsLostTechnology: int
+    :param mineralsUsedActiveForces: _description_
+    :type mineralsUsedActiveForces: int
+    :param mineralsUsedCurrentArmy: _description_
+    :type mineralsUsedCurrentArmy: int
+    :param mineralsUsedCurrentEconomy: _description_
+    :type mineralsUsedCurrentEconomy: int
+    :param mineralsUsedCurrentTechnology: _description_
+    :type mineralsUsedCurrentTechnology: int
+    :param mineralsUsedInProgressArmy: _description_
+    :type mineralsUsedInProgressArmy: int
+    :param mineralsUsedInProgressEconomy: _description_
+    :type mineralsUsedInProgressEconomy: int
+    :param mineralsUsedInProgressTechnology: _description_
+    :type mineralsUsedInProgressTechnology: int
+    :param vespeneCollectionRate: _description_
+    :type vespeneCollectionRate: int
+    :param vespeneCurrent: _description_
+    :type vespeneCurrent: int
+    :param vespeneFriendlyFireArmy: _description_
+    :type vespeneFriendlyFireArmy: int
+    :param vespeneFriendlyFireEconomy: _description_
+    :type vespeneFriendlyFireEconomy: int
+    :param vespeneFriendlyFireTechnology: _description_
+    :type vespeneFriendlyFireTechnology: int
+    :param vespeneKilledArmy: _description_
+    :type vespeneKilledArmy: int
+    :param vespeneKilledEconomy: _description_
+    :type vespeneKilledEconomy: int
+    :param vespeneKilledTechnology: _description_
+    :type vespeneKilledTechnology: int
+    :param vespeneLostArmy: _description_
+    :type vespeneLostArmy: int
+    :param vespeneLostEconomy: _description_
+    :type vespeneLostEconomy: int
+    :param vespeneLostTechnology: _description_
+    :type vespeneLostTechnology: int
+    :param vespeneUsedActiveForces: _description_
+    :type vespeneUsedActiveForces: int
+    :param vespeneUsedCurrentArmy: _description_
+    :type vespeneUsedCurrentArmy: int
+    :param vespeneUsedCurrentEconomy: _description_
+    :type vespeneUsedCurrentEconomy: int
+    :param vespeneUsedCurrentTechnology: _description_
+    :type vespeneUsedCurrentTechnology: int
+    :param vespeneUsedInProgressArmy: _description_
+    :type vespeneUsedInProgressArmy: int
+    :param vespeneUsedInProgressEconomy: _description_
+    :type vespeneUsedInProgressEconomy: int
+    :param vespeneUsedInProgressTechnology: _description_
+    :type vespeneUsedInProgressTechnology: int
+    :param workersActiveCount: _description_
+    :type workersActiveCount: int
+    """
+
     def __init__(
         self,
         foodMade: int,
@@ -41,8 +124,11 @@ class Stats:
         vespeneUsedInProgressTechnology: int,
         workersActiveCount: int,
     ) -> None:
-        self.foodMade = foodMade
-        self.foodUsed = foodUsed
+
+        # This calculation is required for raw data ingestion:
+        self.foodMade = int(foodMade / 4096)
+        self.foodUsed = int(foodUsed / 4096)
+
         self.mineralsCollectionRate = mineralsCollectionRate
         self.mineralsCurrent = mineralsCurrent
         self.mineralsFriendlyFireArmy = mineralsFriendlyFireArmy
