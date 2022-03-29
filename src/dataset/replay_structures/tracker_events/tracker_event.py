54af278012451fb54853c3d72442a0baee0bc114
Andrzej Białecki <kaszanas@interia.pl>
2022-03-29 23:33:32 +0200

    Changed dir structure, TrackerEvent interface added

---
+++ src/dataset/replay_structures/tracker_events/tracker_event.py
@@ -0,0 +1,27 @@
+import abc
+from types import NotImplementedType
+from typing import Dict, Literal
+
+# TODO: from_dict documentation
+
+
+class TrackerEvent(metaclass=abc.ABCMeta):
+    @classmethod
+    def __subclasshook__(cls, subclass: type) -> Literal[True] | NotImplementedType:
+        return (
+            hasattr(subclass, "from_dict")
+            and callable(subclass.from_dict)
+            or NotImplemented
+        )
+
+    @abc.abstractmethod
+    def from_dict(dict: Dict) -> "TrackerEvent":
+        """_summary_
+
+        :param dict: _description_
+        :type dict: Dict
+        :raises NotImplementedError: _description_
+        :return: _description_
+        :rtype: MessageEvent
+        """
+        raise NotImplementedError
