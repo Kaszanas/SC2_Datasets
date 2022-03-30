8aeb9ee9be22afa8e8859ce1d93f16e62fcdb566
Andrzej Białecki <kaszanas@interia.pl>
2022-03-30 22:51:33 +0200

    Deleted unused code, deleted comments

--- test/test_cases/dataset/sc2_egset_dataset_test.py
+++ test/test_cases/dataset/sc2_egset_dataset_test.py
@@ -1,5 +1,7 @@
 import unittest
 
+from src.dataset.sc2_egset_dataset import SC2EGSetDataset
+
 
 class SC2EGSetDatasetTest(unittest.TestCase):
     @classmethod
@@ -7,4 +9,7 @@ class SC2EGSetDatasetTest(unittest.TestCase):
         return super().setUpClass()
 
     def test_loading_replaypacks(self):
+
+        sc2egset_dataset = SC2EGSetDataset
+
         pass
