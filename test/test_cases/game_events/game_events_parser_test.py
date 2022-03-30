c2e2bba3f15c1d715b7aee6f857be816cebdda91
Andrzej Białecki <kaszanas@interia.pl>
2022-03-30 22:03:45 +0200

    Fixed typos, added tests for MessageEventsParser

--- test/test_cases/game_events/game_events_parser_test.py
+++ test/test_cases/game_events/game_events_parser_test.py
@@ -13,13 +13,13 @@ class GameEventsParserTest(unittest.TestCase):
     def setUpClass(cls) -> None:
         cls.test_replay = test_utils.get_specific_asset(filename="test_replay.json")
 
-    def test_tracker_events_parser(self):
+    def test_game_events_parser(self):
 
         with open(self.test_replay) as f:
             loaded_file = json.load(f)
 
             # Iterating over all of the tracker events and verifying
             # If the parsing works correctly:
-            for tracker_event in loaded_file["gameEvents"]:
-                some_parsed_event = GameEventsParser.from_dict(d=tracker_event)
+            for game_event in loaded_file["gameEvents"]:
+                some_parsed_event = GameEventsParser.from_dict(d=game_event)
                 self.assertIsInstance(some_parsed_event, GameEvent)
---
+++ test/test_cases/message_events/message_events_parser_test.py
@@ -0,0 +1,24 @@
+import json
+import unittest
+from src.dataset.replay_structures.message_events.message_event import MessageEvent
+from src.dataset.replay_structures.message_events.message_events_parser import (
+    MessageEventParser,
+)
+import test.test_utils.test_utils as test_utils
+
+
+class MessageEventsParserTest(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.test_replay = test_utils.get_specific_asset(filename="test_replay.json")
+
+    def test_game_events_parser(self):
+
+        with open(self.test_replay) as f:
+            loaded_file = json.load(f)
+
+            # Iterating over all of the tracker events and verifying
+            # If the parsing works correctly:
+            for game_event in loaded_file["messageEvents"]:
+                some_parsed_event = MessageEventParser.from_dict(d=game_event)
+                self.assertIsInstance(some_parsed_event, MessageEvent)
