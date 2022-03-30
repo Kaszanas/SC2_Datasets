58fd2e8467853352b2af1dc898aab28e8c4e39bb
Andrzej Białecki <kaszanas@interia.pl>
2022-03-30 22:51:33 +0200

    Deleted unused code, deleted comments

--- src/dataset/sc2_egset_dataset.py
+++ src/dataset/sc2_egset_dataset.py
@@ -3,7 +3,6 @@ from torch.utils.data._typing import T_co
 from torch.utils.data import Dataset
 from src.dataset.available_replaypacks import AVAILABLE_REPLAYPACKS
 
-from src.dataset.sc2_replay_data import SC2ReplaypackData
 from src.dataset.sc2_replaypack_dataset import SC2ReplaypackDataset
 
 
@@ -25,15 +24,19 @@ class SC2EGSetDataset(Dataset):
         self,
         dataset_unpack_dir: str = "./data/unpack",
         dataset_download_dir: str = "./data/download",
-        urls: List[
+        names_urls: List[
             Tuple[str, str]
         ] = AVAILABLE_REPLAYPACKS,  # This should probably be hardcoded! After all I want this to be a specific dataset.
+        download: bool = True,
         transform=None,
     ):
         self.dataset_download_dir = dataset_download_dir
         self.dataset_unpack_dir = dataset_unpack_dir
+        # TODO: What to do with the transform?
+        # I don't think that it will be used
         self.transform = transform
-        self.list_of_urls = urls  # TODO: maybe rename to self.urls
+        self.names_urls = names_urls
+        self.download = download
 
         # We have received an URL for the dataset
         # and it migth not have been downloaded:
@@ -47,8 +50,14 @@ class SC2EGSetDataset(Dataset):
         self.replaypacks = []
         self.len = 0
 
-        for url in self.urls:
-            replaypack = SC2ReplaypackData(self.dataset_download_dir + "/aaa/")
+        for replaypack_name, url in self.names_urls:
+            replaypack = SC2ReplaypackDataset(
+                replaypack_name=replaypack_name,
+                replaypack_download_dir=self.dataset_download_dir,
+                replaypack_unpack_dir=self.dataset_unpack_dir,
+                url=url,
+                download=self.download,
+            )
             self.replaypacks.append(replaypack)
             self.len += len(replaypack)
 
@@ -56,10 +65,7 @@ class SC2EGSetDataset(Dataset):
         """
         Returns the number of items that are within the dataset
         """
-        # TODO: Implement how to calculate the len of the dataset.
-        # In the case of SC2EGSet this will be the number of files.
-
-        # Get len of list of files from SC2ReplaypackData class
+        # TODO: Verify that this works
         return self.len
 
     def __getitem__(self, index: Any) -> T_co:
@@ -71,15 +77,6 @@ class SC2EGSetDataset(Dataset):
         :return: Returns an item from the dataset.
         :rtype: T_co
         """
-        # TODO: Implement how to get a single item from the dataset!
-        # Most likely this will have to load the JSON file.
-
-        # load and return the specific file that corresponds to the index on the list of files
-
-        # This should return contents of a .json file?
-        # Or maybe different IterableDatasets that coincide with the Lists that .json holds?
-
-        # TODO: Get a specific SC2ReplayData from SC2ReplaypackData
 
         # If the index is negative, treat it as if expressed from the back of the sequence.
         # For example, if index is -1 and lenght is 10, it means we are looking for the last element, which is at index 10 + (-1) = 9
@@ -97,13 +94,3 @@ class SC2EGSetDataset(Dataset):
                 return replaypack[index]
             else:
                 index -= len(replaypack)
-
-    def load_files(self):
-
-        # TODO: Lazily load SC2ReplayData so that it holds the paths
-        # To the .json files holding different info.
-
-        # Load to the list of files
-        # Add the files to the list of files.
-
-        pass
