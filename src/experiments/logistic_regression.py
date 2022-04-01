7bf11e1ef7faea3b9b803e4c8b214c4929192b88
Andrzej Białecki <kaszanas@interia.pl>
2022-04-01 21:05:04 +0200

    Defined TODOs for logistic_regression

--- src/experiments/logistic_regression.py
+++ src/experiments/logistic_regression.py
@@ -1,1 +1,11 @@
-# TODO: Implement Logistic Regression for classification
+from pl_bolts.models.regression import LogisticRegression
+
+# TODO: Import the dataset:
+
+# TODO: Split the dataset into Cross Validation
+
+# TODO: Train the models:
+
+# TODO: Save the model:
+
+# TODO: Save the metrics:
