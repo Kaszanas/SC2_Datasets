659fc1475ced9bcbcc4ef50baba724e78da56cd7
Andrzej Białecki <kaszanas@interia.pl>
2022-04-01 22:51:11 +0200

    Drafted prepare_data in SC2EGSetDataModule

--- src/dataset/lightning_datamodules/sc2_egset_datamodule.py
+++ src/dataset/lightning_datamodules/sc2_egset_datamodule.py
@@ -1,21 +1,36 @@
 from typing import Optional
 import pytorch_lightning as pl
 
+from src.dataset.pytorch_datasets.sc2_egset_dataset import SC2EGSetDataset
+
 
 class SC2EGSetDataModule(pl.LightningDataModule):
     def __init__(
         self,
+        dataset_download_dir: str = "",
+        dataset_unpack_dir: str = "",
         train_transforms=None,
         val_transforms=None,
         test_transforms=None,
         dims=None,
     ):
-        super().__init__(train_transforms, val_transforms, test_transforms, dims)
+        super().__init__()
+
+        self.dataset_download_dir = dataset_download_dir
+        self.dataset_unpack_dir = dataset_unpack_dir
+        self.train_transforms = train_transforms
+        self.val_transforms = val_transforms
+        self.test_transforms = test_transforms
+        self.dims = dims
 
     def prepare_data(self) -> None:
+
         # download, split, etc...
         # only called on 1 GPU/TPU in distributed
-        return super().prepare_data()
+        SC2EGSetDataset(
+            dataset_download_dir=self.dataset_download_dir,
+            dataset_unpack_dir=self.dataset_unpack_dir,
+        )
 
     def setup(self, stage: Optional[str] = None) -> None:
         # make assignments here (val/train/test split)
