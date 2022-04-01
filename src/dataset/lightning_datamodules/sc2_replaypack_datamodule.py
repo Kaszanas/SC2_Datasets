61ec1698ecd422de2c72b11a85e8bd8b27b7af01
Andrzej Białecki <kaszanas@interia.pl>
2022-04-02 01:03:09 +0200

    Added default parameters, drafted replaypack datamodule

--- src/dataset/lightning_datamodules/sc2_replaypack_datamodule.py
+++ src/dataset/lightning_datamodules/sc2_replaypack_datamodule.py
@@ -1,21 +1,44 @@
 from typing import Optional
 import pytorch_lightning as pl
 
+from src.dataset.pytorch_datasets.sc2_replaypack_dataset import SC2ReplaypackDataset
+
 
 class SC2ReplaypackDataModule(pl.LightningDataModule):
     def __init__(
         self,
+        replaypack_name: str,
+        replaypack_unpack_dir: str = "./data/unpack",
+        replaypack_download_dir: str = "./data/unpack",
+        url: str = "",
+        download: bool = True,
         train_transforms=None,
         val_transforms=None,
         test_transforms=None,
         dims=None,
     ):
-        super().__init__(train_transforms, val_transforms, test_transforms, dims)
+        super().__init__()
+
+        self.replaypack_name = replaypack_name
+        self.replaypack_unpack_dir = replaypack_unpack_dir
+        self.replaypack_download_dir = replaypack_download_dir
+        self.url = url
+        self.download = download
+        self.train_transforms = train_transforms
+        self.val_transforms = val_transforms
+        self.test_transforms = test_transforms
+        self.dims = dims
 
     def prepare_data(self) -> None:
         # download, split, etc...
         # only called on 1 GPU/TPU in distributed
-        return super().prepare_data()
+        SC2ReplaypackDataset(
+            replaypack_name=self.replaypack_name,
+            replaypack_unpack_dir=self.replaypack_unpack_dir,
+            replaypack_download_dir=self.replaypack_download_dir,
+            url=self.url,
+            download=self.download,
+        )
 
     def setup(self, stage: Optional[str] = None) -> None:
         # make assignments here (val/train/test split)
