53c74e89120fe95f6e5c172c8da1c4a5f82d9f25
Andrzej Białecki <kaszanas@interia.pl>
2022-04-28 00:04:41 +0200

    Added test_bit_flip_example to test_files

--- test/test_cases/validators/integrity_validator_test.py
+++ test/test_cases/validators/integrity_validator_test.py
@@ -1,4 +1,7 @@
 import unittest
+from src.dataset.utils.sc2_replay_file_info.sc2_replay_file_info import (
+    SC2ReplayFileInfo,
+)
 from src.dataset.validators.integrity_validator import (
     validate_integrity_singleprocess,
     validate_replays_integrity,
@@ -12,10 +15,10 @@ class IntegrityValidatorTest(unittest.TestCase):
     @classmethod
     def setUpClass(cls) -> None:
         cls.not_working_test_replay = Path(
-            test_utils.get_specific_asset(filename="test_replay.json")
+            test_utils.get_specific_asset(filename="test_bit_flip_example.json")
         )
         cls.working_test_replay = Path(
-            test_utils.get_specific_asset(filename="test_replay1.json")
+            test_utils.get_specific_asset(filename="test_replay.json")
         )
         cls.list_of_replays = [cls.not_working_test_replay, cls.working_test_replay]
 
@@ -23,6 +26,10 @@ class IntegrityValidatorTest(unittest.TestCase):
         bad_replays = validate_integrity_singleprocess(
             list_of_replays=self.list_of_replays
         )
+        self.assertIsInstance(next(iter(bad_replays)), SC2ReplayFileInfo)
+        self.assertEqual(len(bad_replays), 1)
 
     def test_multiprocessing_integrity_validator(self):
         bad_replays = validate_replays_integrity(list_of_replays=self.list_of_replays)
+        self.assertIsInstance(next(iter(bad_replays)), SC2ReplayFileInfo)
+        self.assertEqual(len(bad_replays), 1)
