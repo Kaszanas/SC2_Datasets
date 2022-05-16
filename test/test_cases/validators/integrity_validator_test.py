327184b121295fd9dd016e1f48c9bf722dd66261
Andrzej Białecki <kaszanas@interia.pl>
2022-05-16 23:53:36 +0200

    Updated integrity_validator_test, using str

--- test/test_cases/validators/integrity_validator_test.py
+++ test/test_cases/validators/integrity_validator_test.py
@@ -1,9 +1,6 @@
 import json
 import tempfile
 import unittest
-from src.dataset.utils.sc2_replay_file_info.sc2_replay_file_info import (
-    SC2ReplayFileInfo,
-)
 from src.dataset.validators.multiprocess_validator import (
     validate_integrity_mp,
     validate_integrity_persist_mp,
@@ -23,26 +20,20 @@ class IntegrityValidatorTest(unittest.TestCase):
     def setUpClass(cls) -> None:
         cls.not_working_test_replay = Path(
             test_utils.get_specific_asset(filename="test_bit_flip_example.json")
-        )
+        ).as_posix()
         cls.working_test_replay = Path(
             test_utils.get_specific_asset(filename="test_replay.json")
-        )
+        ).as_posix()
         cls.list_of_replays = [
-            SC2ReplayFileInfo(
-                directory=cls.not_working_test_replay.parent,
-                filename=cls.not_working_test_replay.name,
-            ),
-            SC2ReplayFileInfo(
-                directory=cls.working_test_replay.parent,
-                filename=cls.working_test_replay.name,
-            ),
+            cls.working_test_replay,
+            cls.not_working_test_replay,
         ]
 
     def test_singleprocess_integrity_validator(self):
         validated, skip_files = validate_integrity_sp(
             list_of_replays=self.list_of_replays
         )
-        self.assertIsInstance(next(iter(skip_files)), SC2ReplayFileInfo)
+        self.assertIsInstance(next(iter(skip_files)), str)
         self.assertEqual(len(validated), 2)
         self.assertEqual(len(skip_files), 1)
 
@@ -50,7 +41,7 @@ class IntegrityValidatorTest(unittest.TestCase):
         validated, skip_files = validate_integrity_mp(
             list_of_replays=self.list_of_replays, n_workers=2
         )
-        self.assertIsInstance(next(iter(skip_files)), SC2ReplayFileInfo)
+        self.assertIsInstance(next(iter(skip_files)), str)
         self.assertEqual(len(validated), 2)
         self.assertEqual(len(skip_files), 1)
 
@@ -72,7 +63,7 @@ class IntegrityValidatorTest(unittest.TestCase):
                 self.assertEqual(len(validated), 2)
                 self.assertEqual(len(skipped), 1)
 
-        self.assertIsInstance(next(iter(skip_files)), SC2ReplayFileInfo)
+        self.assertIsInstance(next(iter(skip_files)), str)
         self.assertEqual(len(skip_files), 1)
 
     def test_persistent_sp_integrity_validator(self):
@@ -94,5 +85,5 @@ class IntegrityValidatorTest(unittest.TestCase):
                 self.assertEqual(len(validated), 2)
                 self.assertEqual(len(skipped), 1)
 
-        self.assertIsInstance(next(iter(skip_files)), SC2ReplayFileInfo)
+        self.assertIsInstance(next(iter(skip_files)), str)
         self.assertEqual(len(skip_files), 1)
