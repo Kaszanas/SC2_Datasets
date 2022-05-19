b1553be41a76e77f85dccdc890e2c05cd57ce786
Andrzej Białecki <kaszanas@interia.pl>
2022-05-19 22:35:03 +0200

    Added assertions to playerstats_to_dict_test

--- test/test_cases/transforms/pandas/playerstats_to_dict_test.py
+++ test/test_cases/transforms/pandas/playerstats_to_dict_test.py
@@ -1,4 +1,7 @@
 import unittest
+
+import pandas as pd
+
 from src.dataset.replay_data.sc2_replay_data import SC2ReplayData
 from src.dataset.transforms.pandas.player_stats_to_dict import (
     average_playerstats_dataframe,
@@ -14,19 +17,33 @@ class PlayerStatsToDictTest(unittest.TestCase):
         cls.test_replay = test_utils.get_specific_asset(filename="test_replay.json")
         cls.sc2_replay_data = SC2ReplayData.from_file(replay_filepath=cls.test_replay)
 
-    def test_playerstats_average_to_dict(self):
-
-        res_dict = playerstats_average_to_dict(sc2_replay=self.sc2_replay_data)
-
-        # TODO: Make assertions:
-
     def test_playerstats_to_dict(self):
 
         res_dict = playerstats_to_dict(
             tracker_events=self.sc2_replay_data.trackerEvents
         )
 
-        # TODO: Make assertions:
+        # print(res_dict)
+
+        # Type assertions for data:
+        for playerID, feature_dict in res_dict.items():
+            self.assertIsInstance(playerID, str)
+            self.assertIsInstance(feature_dict, dict)
+            for key, feature_list in feature_dict.items():
+                self.assertIsInstance(key, str)
+                self.assertIsInstance(feature_list, list)
+
+    def test_playerstats_average_to_dict(self):
+
+        res_dict = playerstats_average_to_dict(sc2_replay=self.sc2_replay_data)
+
+        # print(res_dict)
+
+        # Type assertions for data:
+        self.assertIsInstance(res_dict, dict)
+        for key, value in res_dict.items():
+            self.assertIsInstance(key, str)
+            self.assertIsInstance(value, dict)
 
     def test_average_playerstats_dataframe(self):
 
@@ -36,4 +53,10 @@ class PlayerStatsToDictTest(unittest.TestCase):
             dataframe = pd.DataFrame.from_dict(df_repr)
             dict_average_repr = average_playerstats_dataframe(playerstats_df=dataframe)
 
-            # TODO: Make assertions:
+            # print(dict_average_repr)
+
+            # Type assertions for the data:
+            self.assertIsInstance(playerID, str)
+            for key, value in dict_average_repr.items():
+                self.assertIsInstance(key, str)
+                self.assertIsInstance(value, float)
