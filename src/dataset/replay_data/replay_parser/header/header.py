1b4c1ea4c4d1666f60a78cf934f6eda8a0d77bfd
Andrzej Szczap <andrzejandrejoo@gmail.com>
2022-06-08 23:58:38 +0200

    Class Header: -duration fields

--- src/dataset/replay_data/replay_parser/header/header.py
+++ src/dataset/replay_data/replay_parser/header/header.py
@@ -3,14 +3,10 @@ from typing import Any, Dict
 
 class Header:
     """
-    Header represents the replay header.
+    Header represents the replay header parameters representation.
 
     :param elapsedGameLoops: Specifies how much game loops (game-engine ticks) the game lasted.
     :type elapsedGameLoops: int
-    :param durationNanoseconds: Specifies how many nanoseconds the game lasted.
-    :type durationNanoseconds: int
-    :param durationSeconds: Specifies how much seconds the game lasted.
-    :type durationSeconds: int
     :param version: Specifies the game version that was used to play the game.
     :type version: str
     """
@@ -27,19 +23,13 @@ class Header:
         """
         return Header(
             elapsedGameLoops=d["elapsedGameLoops"],
-            durationNanoseconds=d["durationNanoseconds"],
-            durationSeconds=d["durationSeconds"],
             version=d["version"],
         )
 
     def __init__(
         self,
         elapsedGameLoops: int,
-        durationNanoseconds: int,
-        durationSeconds: int,
         version: str,
     ) -> None:
         self.elapsedGameLoops = elapsedGameLoops
-        self.durationNanoseconds = durationNanoseconds
-        self.durationSeconds = durationSeconds
         self.version = version
