e3d63a0023363d2546ea60deeb892934333a31eb
Andrzej Szczap <andrzejandrejoo@gmail.com>
2022-06-08 18:15:45 +0200

    Some changes in Class Stats documentation, Some changes in Class PlayerStats documentation

--- src/dataset/replay_data/replay_parser/tracker_events/events/player_stats/player_stats.py
+++ src/dataset/replay_data/replay_parser/tracker_events/events/player_stats/player_stats.py
@@ -8,9 +8,6 @@ from src.dataset.replay_data.replay_parser.tracker_events.tracker_event import (
 )
 
 
-# TODO: Document the docstrings
-
-
 class PlayerStats(TrackerEvent):
     """
     PlayerStats holds information about player economy
@@ -28,11 +25,11 @@ class PlayerStats(TrackerEvent):
     @staticmethod
     def from_dict(d: Dict) -> "PlayerStats":
         """
-        _summary_
+        Static method returning initialized PlayerStats class from a dictionary. This helps with the original JSON parsing.
 
-        :param d: _description_
+        :param d: Specifies a dictionary as available in the JSON file that is a result of pre-processing some .SC2Replay file.
         :type d: Dict
-        :return: _description_
+        :return: Returns an initialized PlayerStats class.
         :rtype: PlayerStats
         """
         return PlayerStats(
--- src/dataset/replay_data/replay_parser/tracker_events/events/player_stats/stats.py
+++ src/dataset/replay_data/replay_parser/tracker_events/events/player_stats/stats.py
@@ -5,8 +5,6 @@ from src.dataset.replay_data.replay_parser.tracker_events.tracker_event import (
 )
 
 
-# TODO: Document the docstrings
-
 
 class Stats(TrackerEvent):
     """
@@ -26,7 +24,7 @@ class Stats(TrackerEvent):
     :type mineralsFriendlyFireEconomy: int
     :param mineralsFriendlyFireTechnology: Specifies how much minerals were lost in friendly fire on technology.
     :type mineralsFriendlyFireTechnology: int
-    :param mineralsKilledArmy: Specifies how much minerals player killed in his oponents army.
+    :param mineralsKilledArmy: Specifies how much minerals a player killed in his oponent's army.
     :type mineralsKilledArmy: int
     :param mineralsKilledEconomy: Specifies how much minerals player killed in his oponents economy.
     :type mineralsKilledEconomy: int
@@ -95,11 +93,11 @@ class Stats(TrackerEvent):
     @staticmethod
     def from_dict(d: Dict) -> "Stats":
         """
-        _summary_
+        Static method returning initialized Stats class from a dictionary. This helps with the original JSON parsing.
 
-        :param d: _description_
+        :param d: Specifies a dictionary as available in the JSON file that is a result of pre-processing some .SC2Replay file.
         :type d: Dict
-        :return: _description_
+        :return: Returns an initialized Stats class.
         :rtype: Stats
         """
         return Stats(
