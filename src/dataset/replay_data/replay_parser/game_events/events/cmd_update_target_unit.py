be25bc8897094b992d84daba59c96129a1281df3
Andrzej Szczap <andrzejandrejoo@gmail.com>
2022-06-09 17:15:23 +0200

    Class UserOptions documentation, some changes in other files

--- src/dataset/replay_data/replay_parser/game_events/events/cmd_update_target_unit.py
+++ src/dataset/replay_data/replay_parser/game_events/events/cmd_update_target_unit.py
@@ -23,11 +23,11 @@ class CmdUpdateTargetUnit(GameEvent):
     @staticmethod
     def from_dict(d: Dict) -> "CmdUpdateTargetUnit":
         """
-        Static method returning initialized CmdUpdateTargetPoint class from a dictionary. This helps with the original JSON parsing.
+        Static method returning initialized CmdUpdateTargetUnit class from a dictionary. This helps with the original JSON parsing.
 
         :param d: Specifies a dictionary as available in the JSON file that is a result of pre-processing some .SC2Replay file.
         :type d: Dict
-        :return: Returns an initialized CmdUpdateTargetPoint class.
+        :return: Returns an initialized CmdUpdateTargetUnit class.
         :rtype: CmdUpdateTargetUnit
         """
         return CmdUpdateTargetUnit(
