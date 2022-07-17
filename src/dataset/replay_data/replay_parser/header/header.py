4c65a1b14443405b22ffbc17a529ee8be87bc333
Andrzej Szczap <andrzejandrejoo@gmail.com>
2022-07-18 00:37:24 +0200

    Header.py doctest

--- src/dataset/replay_data/replay_parser/header/header.py
+++ src/dataset/replay_data/replay_parser/header/header.py
@@ -20,7 +20,40 @@ class Header:
         :type d: Dict[str, Any]
         :return: Returns an initialized Header class.
         :rtype: Header
+
+        **Correct Usage Examples:**
+
+        Using from_dict factory method provides ease of use when parsing a replay pre-processed with SC2InfoExtractorGo_
+
+        This method requires a dictionary representation of data to be passed as a parameter because of the built in json parser provided by the Python standard library.
+
+        _SC2InfoExtractorGo: https://github.com/Kaszanas/SC2InfoExtractorGo
+
+        The use of this method is intended to get header information from the game's json representation.
+
+        >>> elapsedGameLoops = 10000
+        >>> version = "3.12.0.51702"
+        >>> Header(
+        ...    elapsedGameLoops=d["elapsedGameLoops"],
+        ...    version=d["version"])
+
+        >>> assert isinstance(elapsedGameLoops, int)
+        >>> assert elapsedGameLoops > 0
+        >>> assert isinstance(version, str)
+
+        **Incorrect Usage Examples:**
+
+        >>> elapsedGameLoops_wrong = "text"
+        >>> version_wrong = int(2)
+        >>> Header(
+        ...    elapsedGameLoops=elapsedGameLoops_wrong,
+        ...    version=version_wrong)
+        Traceback (most recent call last):
+        ...
+        TypeError: unsupported operand type(s) ...
+
         """
+
         return Header(
             elapsedGameLoops=d["elapsedGameLoops"],
             version=d["version"],
