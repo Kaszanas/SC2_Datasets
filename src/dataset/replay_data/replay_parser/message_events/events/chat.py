82af14460ef6672598f3e61a4f688a9f6b1283f2
Andrzej Szczap <andrzejandrejoo@gmail.com>
2022-07-18 02:13:40 +0200

    Doctests in chat.py file

--- src/dataset/replay_data/replay_parser/message_events/events/chat.py
+++ src/dataset/replay_data/replay_parser/message_events/events/chat.py
@@ -32,7 +32,73 @@ class Chat(MessageEvent):
         :type d: Dict
         :return: Specifies a list of chat parameters like a user id, recipient etc.
         :rtype: Chat
+
+        **Correct Usage Examples:**
+
+        Using from_dict factory method provides ease of use when parsing a replay pre-processed with SC2InfoExtractorGo_
+
+        This method requires a dictionary representation of data to be passed as a parameter because of the built in json
+        parser provided by the Python standard library.
+
+        _SC2InfoExtractorGo: https://github.com/Kaszanas/SC2InfoExtractorGo
+
+        The use of this method is intended to get chat information from the game's json representation.
+
+        >>> chat_dict ={
+        ...      "id": 0,
+        ...      "loop": 185,
+        ...      "recipient": 0,
+        ...      "string": "gl hf",
+        ...      "userid": {
+        ...             "userId": 1}
+        ...      }
+        ...
+        >>> chat_object = Chat.from_dict(d=chat_dict)
+        ...
+        >>> assert isinstance(chat_object, Chat)
+        >>> assert isinstance(chat_object.id, int)
+        >>> assert isinstance(chat_object.loop, int)
+        >>> assert isinstance(chat_object.recipient, int)
+        >>> assert isinstance(chat_object.string, str)
+        >>> assert isinstance(chat_object.userid, int)
+        ...
+        >>> assert chat_object.id == 0
+        >>> assert chat_object.loop == 185
+        >>> assert chat_object.recipient == 0
+        >>> assert chat_object.string == "gl hf"
+        >>> assert chat_object.userid == 1
+        ...
+        >>> assert chat_object.id >= 0
+        >>> assert chat_object.loop >= 0
+        >>> assert chat_object.recipient >= 0
+        >>> assert chat_object.userid >= 0
+
+        **Incorrect Usage Examples:**
+
+        >>> gameOptions_value_wrong = "False"
+        >>> gameSpeed_value_wrong = True
+        >>> isBlizzardMap_value_wrong = "wrong type"
+        >>> mapAuthorName_value_wrong = int(2)
+        >>> mapFileSyncChecksum_value_wrong = str(2)
+        >>> mapSizeX_value_wrong = str(2)
+        >>> mapSizeY_value_wrong = str(2)
+        >>> maxPlayers_value_wrong = str(2)
+
+        >>> GameDescription(
+        ...    gameOptions=gameOptions_value_wrong,
+        ...    gameSpeed=gameSpeed_value_wrong,
+        ...    isBlizzardMap=isBlizzardMap_value_wrong,
+        ...    mapAuthorName=mapAuthorName_value_wrong,
+        ...    mapFileSyncChecksum=mapFileSyncChecksum_value_wrong,
+        ...    mapSizeX=mapSizeX_value_wrong,
+        ...    mapSizeY=mapSizeY_value_wrong,
+        ...    maxPlayers=maxPlayers_value_wrong)
+        Traceback (most recent call last):
+        ...
+        TypeError: unsupported operand type(s) ...
+
         """
+
         return Chat(
             id=d["id"],
             loop=d["loop"],
