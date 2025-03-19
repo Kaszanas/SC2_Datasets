05fe6e37f8c9ea07a2ee7bd81f875910adb365d5
Kaszanas <34846245+Kaszanas@users.noreply.github.com>
2025-03-19 22:28:27 +0100

    test: removed test for deprecated function

--- tests/test_cases/utils/download_utils_test.py
+++ tests/test_cases/utils/download_utils_test.py
@@ -6,29 +6,10 @@ from pathlib import Path
 import pytest
 
 from sc2_datasets.utils.download_utils import (
-    download_and_unpack_replaypack,
     download_replaypack,
 )
 from tests.settings_test import TEST_SYNTHETIC_REPLAYPACKS
 from tests.test_utils.test_utils import get_test_output_dir
-
-"""
-    **Incorrect Usage Examples:**
-
-    >>> wrong_type_object = int(2)
-    >>> download_and_unpack_replaypack_object = download_and_unpack_replaypack(
-    ...            replaypack_download_dir=wrong_type_object,
-    ...            replaypack_unpack_dir="/directory/replaypack_unpack_dir",
-    ...            replaypack_name="replaypack_name",
-    ...            url="url")
-    Traceback (most recent call last):
-    ...
-    TypeError: unsupported operand type(s) ...
-
-    If you don't set parameters or paste incorect parameters' type.
-
-    If the parameters aren't set
-"""
 
 
 @pytest.mark.minor
@@ -56,12 +37,4 @@ class DownloadUtilsTest(unittest.TestCase):
             destination_dir=self.download_dir_path,
             replaypack_name=TEST_SYNTHETIC_REPLAYPACKS[0][0],
             replaypack_url=TEST_SYNTHETIC_REPLAYPACKS[0][1],
-        )
-
-    def test_download_and_unpack_replaypack(self):
-        self.unpacked = download_and_unpack_replaypack(
-            replaypack_download_dir=self.download_dir_path,
-            replaypack_unpack_dir=self.unpack_dir_path,
-            replaypack_name=TEST_SYNTHETIC_REPLAYPACKS[0][0],
-            url=TEST_SYNTHETIC_REPLAYPACKS[0][1],
         )
