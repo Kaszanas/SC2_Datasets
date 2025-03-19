9cd6865116e91a0d71095ddbc60d7aaec6913a37
Kaszanas <34846245+Kaszanas@users.noreply.github.com>
2025-03-19 22:27:15 +0100

    feat: multithreading zip unpacking with progressbars

--- src/sc2_datasets/utils/zip_utils.py
+++ src/sc2_datasets/utils/zip_utils.py
@@ -2,18 +2,44 @@
 import logging
 import math
 import zipfile
-from concurrent.futures import ProcessPoolExecutor
+from concurrent.futures import ThreadPoolExecutor
 from pathlib import Path
 from typing import List
 
 from tqdm import tqdm
 
 
+class UnpackZipFileArguments:
+    """
+    Helper class for passing arguments to a multi-threaded unpacking function.
+
+    Parameters
+    ----------
+    zip_path : Path
+        Specifies the path to the archive file that will be extracted.
+    filenames : List[str]
+        Specifies a list of the filenames which are within the archive\
+        and will be extracted.
+    path_to_extract : Path
+        Specifies the path to which the files will be extracted to.
+    """
+
+    def __init__(
+        self,
+        chunk_id: int,
+        zip_path: Path,
+        filenames: List[str],
+        path_to_extract: Path,
+    ):
+        self.chunk_id = chunk_id
+        self.zip_path = zip_path
+        self.filenames = filenames
+        self.path_to_extract = path_to_extract
+
+
 # REVIEW: Check this:
 def unpack_chunk(
-    zip_path: Path,
-    filenames: List[str],
-    path_to_extract: Path,
+    unpack_arguments: UnpackZipFileArguments,
 ) -> None:
     """
     Helper function for unpacking a chunk of files from an archive.
@@ -48,10 +74,21 @@ def unpack_chunk(
     >>> assert isinstance(path_to_extract, str)
     """
 
-    with zipfile.ZipFile(zip_path, "r") as zip_file:
-        for filename in filenames:
+    with zipfile.ZipFile(unpack_arguments.zip_path, "r") as zip_file:
+        for filename in tqdm(
+            unpack_arguments.filenames,
+            desc=f"W {unpack_arguments.chunk_id} Extracting from {unpack_arguments.zip_path.name}:",
+            unit="file",
+        ):
+            # Check if the file already exists:
+            maybe_unpacked_file = Path(
+                unpack_arguments.path_to_extract, filename
+            ).resolve()
+            if maybe_unpacked_file.exists():
+                continue
+
             try:
-                zip_file.extract(filename, path_to_extract)
+                zip_file.extract(member=filename, path=unpack_arguments.path_to_extract)
             except zipfile.error as e:
                 logging.error(
                     f"zipfile error was raised: {e}",
@@ -119,22 +156,39 @@ def unpack_zipfile(
 
     file_list: List[str] = []
     path_to_extract = Path(destination_dir, subdir).resolve()
+    if not path_to_extract.exists():
+        path_to_extract.mkdir(parents=True)
+
+    file_list = []
     with zipfile.ZipFile(zip_path, "r") as zip_file:
         # Checking the existence of the extraction output directory
         # If it doesn't exist it will be created:
-        if not path_to_extract.exists():
-            path_to_extract.mkdir(parents=True)
-
         file_list = zip_file.namelist()
 
     chunksize = math.ceil(len(file_list) / n_workers)
 
-    with ProcessPoolExecutor(n_workers) as exe:
-        for index in tqdm(
-            range(0, len(file_list), chunksize),
-            desc=f"Extracting {destination_dir.stem}: ",
-        ):
-            filenames = file_list[index : (index + chunksize)]
-            _ = exe.submit(unpack_chunk, zip_path, filenames, path_to_extract)
+    # Get chunks to unpack with threads:
+    file_chunks = []
+    for index in range(0, len(file_list), chunksize):
+        file_chunks.append(file_list[index : (index + chunksize)])
+
+    # Get the arguments for the unpacking function:
+    unpack_arguments = []
+    for index, chunk in enumerate(file_chunks):
+        unpack_arguments.append(
+            UnpackZipFileArguments(
+                chunk_id=index,
+                zip_path=zip_path,
+                filenames=chunk,
+                path_to_extract=path_to_extract,
+            )
+        )
+
+    with ThreadPoolExecutor(
+        max_workers=n_workers,
+        initializer=tqdm.set_lock,
+        initargs=(tqdm.get_lock(),),
+    ) as exe:
+        _ = exe.map(unpack_chunk, unpack_arguments)
 
     return path_to_extract
