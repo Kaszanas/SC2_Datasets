11eb686e1bdc8bece549738d9e366c2cf835f5f2
Kaszanas <34846245+Kaszanas@users.noreply.github.com>
2025-12-03 18:20:10 +0100

    test: fixing after class rename

--- src/sc2_datasets/lightning/sc2_egset_datamodule.py
+++ src/sc2_datasets/lightning/sc2_egset_datamodule.py
@@ -8,11 +8,11 @@ from sc2_datasets.available_replaypacks import (
 )
 from sc2_datasets.lightning.datamodules.sc2_datamodule import (
     SC2DataModule,
-    SC2SingleJSONDataModule,
+    SC2DataModuleSingleJSON,
 )
 
 
-class SC2EGSetDataModuleSingleJSON(SC2SingleJSONDataModule):
+class SC2EGSetDataModuleSingleJSON(SC2DataModuleSingleJSON):
     def __init__(
         self,
         dataset_name: str = SC2EGSET_SINGLE_JSON.name,
